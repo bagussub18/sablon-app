4d90d503bacc69d09f323d4a24479f5be473bcd6
bagussub18 <bagussub18@gmail.com>
2026-01-22 23:18:39 +0700

    add

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,11 +2,11 @@ const mysql = require('mysql2');
 
 // Konfigurasi koneksi database
 const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  port: process.env.DB_PORT,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT
 });
 // const db = mysql.createConnection({
 //   host: 'localhost',
