7cadcdc51ad8c2a5c05a5ee2e1e3c1c27edf7f0c
bagussub18 <bagussub18@gmail.com>
2026-01-23 15:48:29 +0700

    add

--- backend/middleware/upload.js
+++ backend/middleware/upload.js
@@ -1,6 +1,6 @@
-import multer from 'multer';
-import { CloudinaryStorage } from 'multer-storage-cloudinary';
-import cloudinary from '../config/cloudinary.js';
+const multer = require ('multer');
+const CloudinaryStorage  = require ('multer-storage-cloudinary');
+const cloudinary = require ('../config/cloudinary.js');
 
 const storage = new CloudinaryStorage({
   cloudinary,
